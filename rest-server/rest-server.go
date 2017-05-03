45335a32eb9de2bdae5e2c2dc2d7422d02c92415
DalaiLlaama <glamperd@gmail.com>
2017-05-03 16:19:43 +1000

    save - work in progress

--- rest-server/rest-server.go
+++ rest-server/rest-server.go
@@ -2,12 +2,29 @@ package main
 
     import (
         "fmt"
-        "github.com/julienschmidt/httprouter"
+        "golang-sample/github.com/julienschmidt/httprouter"
         "log"
         "net/http"
+		"os"
+		"encoding/json"
     )
+	
+	type Article struct {
+		id int
+		title string
+		date string
+		body string
+		tags []string
+	}
+	
+	type ArticleList struct {
+		articles []Article
+	}
+	
+	var articleList ArticleList
 
     func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	    fmt.Println("path", r.URL.Path)
         fmt.Fprint(w, "Welcome!\n")
     }
 
@@ -19,6 +36,7 @@ package main
         uid := ps.ByName("uid")
         fmt.Fprintf(w, "you are get user %s", uid)
     }
+
 
     func modifyuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
         uid := ps.ByName("uid")
@@ -36,6 +54,35 @@ package main
         fmt.Fprintf(w, "you are add user %s", uid)
     }
 
+    func getTags(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        uid := ps.ByName("tagName")
+		date := ps.ByName("date")
+		
+		
+        fmt.Fprintf(w, "you are get user %s", uid)
+    }
+	
+	func loadArticles() ArticleList {
+		articleFile := "articles.json"
+		fin, err := os.Open(articleFile) // read only. That's all this needs
+		if err != nil {
+			fmt.Println(articleFile, err)
+			return nil
+		}
+		defer fin.Close()
+		buf := make([]byte, 4096)
+		var articles articleList 
+		n, _ := fin.Read(buf)
+		err = json.Unmarshal(buf, &articles)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		
+		fmt.Println("articles", len(articles))
+		return articles
+	}
+
     func main() {
         router := httprouter.New()
         router.GET("/", Index)
@@ -45,6 +92,9 @@ package main
         router.POST("/adduser/:uid", adduser)
         router.DELETE("/deluser/:uid", deleteuser)
         router.PUT("/moduser/:uid", modifyuser)
+		router.GET("/tag/{tagName}/{date}", getTags)
+		
+		articleList = loadArticles()
 
         log.Fatal(http.ListenAndServe(":8080", router))
     }
